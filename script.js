2fee5dc1d55c6a5603197f9f4199f510f39e92d8
Kevin Banza <kevinbanza@gmail.com>
2019-06-21 14:42:50 +0100

    f-tech initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// **************************************
+// ***     Designed And Developed     ***
+// **               By                ***
+// ***          Kevin Banza           ***
+// **************************************
+
+const nav_benefits = document.querySelector('#navbtn-benefits');
+const nav_zintec = document.querySelector('#navbtn-zintec');
+const nav_contact = document.querySelector('#navbtn-contact');
+const nav_home = document.querySelector('#navbtn-home');
+const contactResultbtn = document.querySelector('#contactResult-btn');
+const section_benefits = document.querySelector('#benefits');
+const section_zintec = document.querySelector('#zintec');
+const section_contact = document.querySelector('#contact');
+const section_contactResult = document.querySelector('#contactResult');
+const section_home = document.querySelector('#home');
+console.log('Website Made By Kevin Banza');
+console.log('Kevin Banza : http://kevinbanza.com/');
+
+setActive(section_home, nav_home);
+
+function removeClass(target, classToRemove) {
+    if (target.classList.contains(classToRemove)) {
+        target.classList.remove(classToRemove);
+    }
+}
+
+function addClass(target, classToRemove) {
+    if (!target.classList.contains(classToRemove)) {
+        target.classList.add(classToRemove);
+    }
+}
+
+function setActive(target, nav) {
+    removeClass(section_home, 'active');
+    removeClass(section_benefits, 'active');
+    removeClass(section_zintec, 'active');
+    removeClass(section_contact, 'active');
+    removeClass(section_contactResult, 'active');
+    addClass(target, 'active');
+    if (nav != null) {
+        setNavActive(nav);
+    }
+}
+
+function setNavActive(target) {
+    removeClass(nav_home, 'active');
+    removeClass(nav_benefits, 'active');
+    removeClass(nav_zintec, 'active');
+    removeClass(nav_contact, 'active');
+    addClass(target, 'active');
+}
+
+
+nav_home.addEventListener('click', function(e) {
+    setActive(section_home, nav_home);
+});
+nav_benefits.addEventListener('click', function(e) {
+    setActive(section_benefits, nav_benefits);
+});
+nav_zintec.addEventListener('click', function(e) {
+    setActive(section_zintec, nav_zintec);
+});
+nav_contact.addEventListener('click', function(e) {
+    setActive(section_contact, nav_contact);
+});
+contactResultbtn.addEventListener('click', function(e) {
+    setActive(section_contact, nav_contact);
+});
---
+++ videoScript.js
@@ -0,0 +1,46 @@
+// **************************************
+// ***     Designed And Developed     ***
+// **               By                ***
+// ***          Kevin Banza           ***
+// **************************************
+
+const dropdownProductMenu = document.querySelector('div.actions>select');
+const videoPlayer = document.querySelector('#video-player');
+const pp = document.querySelector('#play-pause');
+const vid = document.querySelector('video');
+
+videoPlayer.src = 'assets/F-Tech_Standard_C.mp4';
+
+function changeVideo(product) {
+    switch (product.value) {
+        case "standard":
+            videoPlayer.src = 'assets/F-Tech_Standard_C.mp4';
+            break;
+        case "premium":
+            videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
+            break;
+        case "advertisment":
+            videoPlayer.src = 'assets/F-tech_Advertising.mp4';
+            break;
+        default:
+            videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
+            break;
+    }
+}
+
+function playPause(ele) {
+    if (ele.classList.contains("playing")) {
+        removeClass(ele, 'playing');
+        addClass(ele, 'paused');
+        ele.innerHTML = 'Play'
+        vid.pause();
+    } else {
+        removeClass(ele, 'paused');
+        addClass(ele, 'playing');
+        ele.innerHTML = 'Pause'
+        vid.play();
+    }
+}
+pp.addEventListener('click', function(e) {
+    playPause(pp);
+})
